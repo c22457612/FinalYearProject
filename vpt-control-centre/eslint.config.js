6398e823f0e40dd819b4d6ce89bb333b652e6d56
c22457612 <c22457612@mytudublin.ie>
2026-02-19 16:34:22 +0000

    chore: add eslint and semgrep config

---
+++ vpt-control-centre/eslint.config.js
@@ -0,0 +1,25 @@
+﻿const security = require("eslint-plugin-security");
+const noUnsanitized = require("eslint-plugin-no-unsanitized");
+
+module.exports = [
+  {
+    files: ["**/*.js"],
+    ignores: ["node_modules/**"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "script"
+    },
+    plugins: {
+      security,
+      "no-unsanitized": noUnsanitized
+    },
+    rules: {
+      // Baseline security rules
+      ...security.configs.recommended.rules,
+
+      // DOM XSS sink detection
+      "no-unsanitized/method": "warn",
+      "no-unsanitized/property": "warn"
+    }
+  }
+];
